cba3ebc84e4ca371f0567b5ae2444a6ac8f988ac
kxusx <khush99199@gmail.com>
2022-02-20 23:30:00 +0530

    1st

---
+++ The-Boat-Game/main.js
@@ -0,0 +1,253 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import Stats from 'three/examples/jsm/libs/stats.module.js';
+
+import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { Water } from 'three/examples/jsm/objects/Water.js';
+import { Sky } from 'three/examples/jsm/objects/Sky.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+let container, stats; // dont need
+let camera, scene, renderer;
+let controls, water, sun, mesh;
+
+const loader = new GLTFLoader();
+// loader.load("models/player/scene.gltf",function(gltf){
+  
+//   scene.add(gltf.scene);
+//   gltf.scene.scale.set(0.005,0.005,0.005) // scale here
+//   gltf.scene.position.set(0,0,0);
+  
+// })
+
+class Boat {
+  constructor(){
+    loader.load("models/player/scene.gltf",(gltf)=>{
+      scene.add( gltf.scene )
+      gltf.scene.scale.set(0.005,0.005,0.005) // scale here
+      gltf.scene.position.set(0,0,0);
+      gltf.scene.rotation.y = 1.75
+
+      this.boat = gltf.scene
+      this.speed = {
+        vel: 0,
+        rot: 0
+      }
+    })
+  }
+
+  stop(){ 
+    this.speed.vel = 0
+    this.speed.rot = 0
+  }
+
+  update(){
+    if(this.boat){
+      this.boat.rotation.y += this.speed.rot
+      this.boat.translateZ(this.speed.vel)
+    }
+  }
+}
+
+const boat = new Boat()
+
+//----------------------------------------------------------------------------------
+
+class Trash{
+    constructor(){
+      loader.load("models/coin/scene.gltf",(gltf)=>{
+        scene.add( gltf.scene )
+        gltf.scene.scale.set(10,10,10) // scale here
+        gltf.scene.position.set(0,1,0);
+      })
+    }
+}
+let trash = new Trash()
+
+
+init();
+animate();
+
+function init() {
+
+  // container = document.getElementById('container'); // dont need
+
+  //
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 20000);
+  camera.position.set(30, 30, 100);
+
+  //
+
+  sun = new THREE.Vector3();
+
+  // Water
+
+  const waterGeometry = new THREE.PlaneGeometry(10000, 10000);
+
+  water = new Water(
+    waterGeometry,
+    {
+      textureWidth: 512,
+      textureHeight: 512,
+      waterNormals: new THREE.TextureLoader().load('textures/waternormals.jpg', function (texture) {
+
+        texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+      }),
+      sunDirection: new THREE.Vector3(),
+      sunColor: 0xffffff,
+      waterColor: 0x001e0f,
+      distortionScale: 3.7,
+      fog: scene.fog !== undefined
+    }
+  );
+
+  water.rotation.x = - Math.PI / 2;
+
+  scene.add(water);
+
+  // Skybox
+
+  const sky = new Sky();
+  sky.scale.setScalar(10000);
+  scene.add(sky);
+
+  const skyUniforms = sky.material.uniforms;
+
+  skyUniforms['turbidity'].value = 10;
+  skyUniforms['rayleigh'].value = 2;
+  skyUniforms['mieCoefficient'].value = 0.005;
+  skyUniforms['mieDirectionalG'].value = 0.8;
+
+  const parameters = {
+    elevation: 2,
+    azimuth: 180
+  };
+
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+  function updateSun() {
+
+    const phi = THREE.MathUtils.degToRad(90 - parameters.elevation);
+    const theta = THREE.MathUtils.degToRad(parameters.azimuth);
+
+    sun.setFromSphericalCoords(1, phi, theta);
+
+    sky.material.uniforms['sunPosition'].value.copy(sun);
+    water.material.uniforms['sunDirection'].value.copy(sun).normalize();
+
+    scene.environment = pmremGenerator.fromScene(sky).texture;
+
+  }
+
+  updateSun();
+
+  //
+// dont need
+  // const geometry = new THREE.BoxGeometry(30, 30, 30);
+  // const material = new THREE.MeshStandardMaterial({ roughness: 0 });
+
+  // mesh = new THREE.Mesh(geometry, material);
+  // scene.add(mesh);
+
+  // //
+
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.maxPolarAngle = Math.PI * 0.495;
+  controls.target.set(0, 10, 0);
+  controls.minDistance = 40.0;
+  controls.maxDistance = 200.0;
+  controls.update();
+
+  //
+  // dont need
+  // stats = new Stats();
+  // container.appendChild(stats.dom);
+
+  // GUI
+  // dont need
+  // const gui = new GUI();
+  // const folderSky = gui.addFolder('Sky');
+  // folderSky.add(parameters, 'elevation', 0, 90, 0.1).onChange(updateSun);
+  // folderSky.add(parameters, 'azimuth', - 180, 180, 0.1).onChange(updateSun);
+  // folderSky.open();
+
+  const waterUniforms = water.material.uniforms;
+
+  // const folderWater = gui.addFolder('Water');
+  // folderWater.add(waterUniforms.distortionScale, 'value', 0, 8, 0.1).name('distortionScale');
+  // folderWater.add(waterUniforms.size, 'value', 0.1, 10, 0.1).name('size');
+  // folderWater.open();
+
+  //
+
+  window.addEventListener('resize', onWindowResize);
+
+  window.addEventListener( 'keydown', function(e){
+    if(e.key == "w"){
+      boat.speed.vel = 1
+    }
+    if(e.key == "s"){
+      boat.speed.vel = -1
+    }
+    if(e.key == "d"){
+      boat.speed.rot = -0.1
+    }
+    if(e.key == "a"){
+      boat.speed.rot = 0.1
+    }
+  })
+
+  window.addEventListener( 'keyup', function(e){
+    boat.stop()
+  })
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function animate() {
+
+  requestAnimationFrame(animate);
+  render();
+  boat.update();
+  // dont need
+  // stats.update();
+
+}
+
+function render() {
+
+  // const time = performance.now() * 0.001;
+
+  // dont need
+  // mesh.position.y = Math.sin(time) * 20 + 5;
+  // mesh.rotation.x = time * 0.5;
+  // mesh.rotation.z = time * 0.51;
+
+  water.material.uniforms['time'].value += 1.0 / 60.0;
+
+  renderer.render(scene, camera);
+
+}
