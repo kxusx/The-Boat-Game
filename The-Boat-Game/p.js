31abcb06414a15cc0c8de0b33b6e9777515eacf4
kxusx <khush99199@gmail.com>
2022-02-22 12:12:35 +0530

    enemey-player collisions

--- The-Boat-Game/p.js
+++ The-Boat-Game/p.js
@@ -38,11 +38,7 @@ class Boat {
       gltf.scene.position.set(0, 0, 0);
       gltf.scene.rotation.y = 1.75
       console.log("E");
-      this.position = {
-        x: gltf.scene.position.x,
-        y: gltf.scene.position.y,
-        z: gltf.scene.position.z
-      }
+
 
       this.boat = gltf.scene
       this.speed = {
@@ -58,6 +54,7 @@ class Boat {
     };
     this.lasers = new Array();
     this.vels = new Array();
+    this.times = new Array();
   }
 
   stop() {
@@ -71,23 +68,24 @@ class Boat {
       this.boat.translateZ(this.speed.vel)
       let prev = new Vector3(0, -1, 0);
       prev.applyAxisAngle(new Vector3(0, 1, 0), this.boat.rotation.y);
-      
-      this.position.x = this.boat.position.x
-      this.position.y = this.boat.position.y
-      this.position.z = this.boat.position.z
+
+      // this.position.x = this.boat.position.x
+      // this.position.y = this.boat.position.y
+      // this.position.z = this.boat.position.z
     }
 
     let toDel = new Array();
     for (let i = 0; i < this.lasers.length; i++) {
-      this.lasers[i].position.set(this.lasers[i].position.x+this.vels[i][0], this.lasers[i].position.y+this.vels[i][1], this.lasers[i].position.z+this.vels[i][2]);
-      if (this.lasers[i].position.z < -60) {
+      this.lasers[i].position.set(this.lasers[i].position.x + this.vels[i][0], this.lasers[i].position.y + this.vels[i][1], this.lasers[i].position.z + this.vels[i][2]);
+      if (new Date().getTime() - this.times[i] > 5000) {
         scene.remove(this.lasers[i]);
         toDel.push(i);
       }
     }
-    for(var i=0; i<toDel.length; i++){
-        this.lasers.splice(toDel[i], 1);
-        this.vels.splice(toDel[i], 1);
+    for (var i = 0; i < toDel.length; i++) {
+      this.lasers.splice(toDel[i], 1);
+      this.vels.splice(toDel[i], 1);
+      this.times.splice(toDel[i], 1);
     }
   }
 
@@ -97,13 +95,14 @@ class Boat {
       return;
     this.bullet.lastShot = new Date().getTime();
     var shot = new THREE.Mesh(this.bullet.geom, this.bullet.mat);
-    shot.position.set(this.boat.position.x , this.boat.position.y + 4, this.boat.position.z+5);
+    shot.position.set(this.boat.position.x, this.boat.position.y + 4, this.boat.position.z + 5);
     // console.log(this.boat.rotation.y)
-    shot.rotation.x = -pi/2
+    shot.rotation.x = -pi / 2
     shot.rotation.z = this.boat.rotation.y
 
     this.lasers.push(shot);
-    this.vels.push([ Math.sin(this.boat.rotation.y), 0, Math.cos(this.boat.rotation.y)])
+    this.vels.push([Math.sin(this.boat.rotation.y), 0, Math.cos(this.boat.rotation.y)])
+    this.times.push(new Date().getTime())
     scene.add(shot);
   }
 }
@@ -139,28 +138,31 @@ const TRASH_COUNT = 100
 // ----------------------------------------------------------------------------------
 
 class Enemy {
-  constructor() {
-    loader.load("models/enemy/scene.gltf", (gltf) => {
-      scene.add(gltf.scene);
-      gltf.scene.scale.set(0.75, 0.75, 0.75); // scale here
-      gltf.scene.position.set(10, 1, 10);
-      // gltf.scene.rotation.y = 1.5;
-      // console.log(gltf.size)
-      this.enemy = gltf.scene;
-      console.log(this.enemy.position);
-      this.speed = {
-        vel: 0.001,
-        rot: 10,
-      };
-    });
+  constructor(_scene) {
+
+    scene.add(_scene);
+    _scene.scale.set(0.75, 0.75, 0.75); // scale here
+    _scene.position.set(random(-500, 500), 1, random(-500, 500));
+    // gltf.scene.rotation.y = 1.5;
+    // console.log(gltf.size)
+    this.enemy = _scene;
+
+    this.speed = {
+      vel: 0.001,
+      rot: 10,
+    };
+
     this.bullet = {
       geom: new THREE.CylinderGeometry(1, 0.1, 2, 32),
-      mat: new THREE.MeshBasicMaterial({ color: 0xDC143C }),
-      cooldown: 500,
+      mat: new THREE.MeshBasicMaterial({ color: 0x0000FF }),
+      cooldown: 2500,
       lastShot: 0
     };
     this.lasers = new Array();
     this.vels = new Array();
+    this.times = new Array();
+    this.xs = new Array();
+    this.zs = new Array();
   }
 
   stop() {
@@ -190,12 +192,66 @@ class Enemy {
       const arccosine = Math.acos(cosine);
       this.enemy.rotation.y = arccosine;
 
+      let toDel = new Array();
+      for (let i = 0; i < this.lasers.length; i++) {
+        // this.lasers[i].position.set(this.lasers[i].position.x+this.vels[i][0], this.lasers[i].position.y+this.vels[i][1], this.lasers[i].position.z+this.vels[i][2]);
+        this.lasers[i].position.set(this.lasers[i].position.x + this.xs[i] * 0.01, this.lasers[i].position.y + 0, this.lasers[i].position.z + this.zs[i] * 0.01);
+        if (new Date().getTime() - this.times[i] > 5000) {
+          scene.remove(this.lasers[i]);
+          toDel.push(i);
+        }
+      }
+      console.log(toDel.length)
+      for (var i = 0; i < toDel.length; i++) {
+        this.lasers.splice(toDel[i], 1);
+        this.vels.splice(toDel[i], 1);
+        this.times.splice(toDel[i], 1);
+        this.xs.splice(toDel[i], 1);
+        this.zs.splice(toDel[i], 1);
+      }
+
       // console.log(enem)
+    }
+  }
+  shoot() {
+    if (this.enemy) {
+      if (new Date().getTime() - this.bullet.lastShot < this.bullet.cooldown)
+        return;
+      this.bullet.lastShot = new Date().getTime();
+      var shot = new THREE.Mesh(this.bullet.geom, this.bullet.mat);
+      shot.position.set(this.enemy.position.x, this.enemy.position.y + 4, this.enemy.position.z + 5);
+      console.log("enemy shooted")
+      shot.rotation.x = -pi / 2
+      shot.rotation.z = this.enemy.rotation.y
+
+      this.lasers.push(shot);
+      this.vels.push([Math.sin(this.enemy.rotation.y), 0, Math.cos(this.enemy.rotation.y)])
+      this.times.push(new Date().getTime())
+      this.xs.push(boat.boat.position.x - this.enemy.position.x)
+      this.zs.push(boat.boat.position.z - this.enemy.position.z)
+
+      scene.add(shot);
+    }
+  }
+
+  removeEnemyBullets() {
+    for (var i = 0; i < this.lasers.length; i++) {
+      scene.remove(this.lasers[i]);
     }
   }
 }
 
-const enemy = new Enemy()
+let enemyModel = null
+// demoss this by making a flag
+async function createEnemy() {
+  if (!enemyModel) {
+    enemyModel = await loadModel("models/enemy/scene.gltf")
+  }
+  return new Enemy(enemyModel.clone())
+}
+
+let enemys = []
+const ENEMY_COUNT = 2
 
 init();
 animate();
@@ -299,6 +355,11 @@ async function init() {
     trashes.push(trash)
   }
 
+  for (let i = 0; i < ENEMY_COUNT; i++) {
+    const enemy = await createEnemy()
+    enemys.push(enemy)
+  }
+
   window.addEventListener('resize', onWindowResize);
 
   window.addEventListener('keydown', function (e) {
@@ -336,18 +397,19 @@ function onWindowResize() {
 
 }
 
-function isColliding(obj1, obj2) {
+function isTrashColliding(obj1, obj2) {
   return (
     Math.abs(obj1.position.x - obj2.position.x) < 15 &&
     Math.abs(obj1.position.z - obj2.position.z) < 10
   )
 }
 
-function checkCollisions() {
+function checkTrashCollisions() {
   if (boat.boat) {
     trashes.forEach(trash => {
       if (trash.trash) {
-        if (isColliding(boat.boat, trash.trash)) {
+        console.log("e")
+        if (isTrashColliding(boat.boat, trash.trash)) {
           scene.remove(trash.trash)
         }
       }
@@ -355,13 +417,59 @@ function checkCollisions() {
   }
 }
 
+function isEnemyColliding(obj1, obj2) {
+  return (
+    Math.abs(obj1.position.x - obj2.position.x) < 20 &&
+    Math.abs(obj1.position.z - obj2.position.z) < 20
+  )
+}
+
+function checkEnemyCollisions() {
+  if (boat.boat) {
+    enemys.forEach(enemy => {
+      if (enemy.enemy) {
+        if (isEnemyColliding(boat.boat, enemy.enemy)) {
+          scene.remove(enemy.enemy)
+          enemys[enemys.indexOf(enemy)].removeEnemyBullets()
+          enemys.splice(enemys.indexOf(enemy), 1)
+        }
+      }
+    })
+  }
+}
+
+function checkPlayerBulletCollisions() {
+  if (boat.boat) {
+    enemys.forEach(enemy => {
+      if (enemy.enemy) {
+        enemy.lasers.forEach(laser => {
+          if (laser.laser) {
+            if (isTrashColliding(laser.laser, boat.boat)) {
+              scene.remove(laser.laser)
+              boat.removePlayerBullet()
+              enemys[enemys.indexOf(enemy)].removeEnemyBullet()
+              enemys.splice(enemys.indexOf(enemy), 1)
+            }
+          }
+        })
+      }
+    })
+  }
+}
+
+
 function animate() {
 
   requestAnimationFrame(animate);
   render();
   boat.update();
-  enemy.update();
-  checkCollisions();
+  for (let i = 0; i < enemys.length; i++) {
+    enemys[i].update()
+    enemys[i].shoot()
+  }
+  checkTrashCollisions();
+  checkEnemyCollisions();
+
 
 
 
